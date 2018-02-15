81b0fa4ceb9df99f355113a6ccc1aa4cf0e874f7
GeorgeZJChen <35523752+GeorgeZJChen@users.noreply.github.com>
2018-02-15 18:13:45 +0000

    Add files via upload

---
+++ 201610111542/js/login.js
@@ -0,0 +1,39 @@
+(function() {
+
+  var url = "http://192.168.56.101/";
+
+  $(document).ready(function(){
+
+    $('#login_submit').click(function(){
+      var username = $('#login_username').val();
+      var password = $('#login_password').val();
+      console.log(username+", "+password);
+      $.post(url + "test.php", {
+        email: username,
+        password: password
+      }, function(data) {
+        if(data == "Login failed"){
+          alert(data);
+        }else{
+          $('#main_container').show();
+          $('.login-container').fadeOut(300, function(){
+            $(this).hide();
+          });
+        }
+        console.log(data);
+      });
+
+
+  });
+
+  });
+
+
+
+
+
+
+
+
+
+}.call(this));
