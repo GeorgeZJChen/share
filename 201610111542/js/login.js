57937ce621ea4aab5e72d9e35a078a71473a5e68
GeorgeZJChen <georgechenzj@outlook.com>
2018-02-15 23:45:12 +0000

    aaa

--- 201610111542/js/login.js
+++ 201610111542/js/login.js
@@ -4,32 +4,47 @@
 
   $(document).ready(function(){
 
-    $('#login_submit').click(function(){
-      var username = $('#login_username').val();
-      var password = $('#login_password').val();
-      console.log(username+", "+password);
-      $.post(url + "test.php", {
-        email: username,
-        password: password
-      }, function(data) {
-        if(data == "Login failed"){
-          alert(data);
-        }else{
-          $('#header_username').html(username);
-          $('#main_container').show();
-          $('.login-container').fadeOut(300, function(){
-            $(this).hide();
+    var catch_key = function(event){
+　　　　if(event.keyCode == 13){
+          sign_in(function(){
+            $(document).unbind(event, catch_key);
           });
-        }
-        console.log(data);
-      });
+　　　　}
+　　}
+    $(document).keydown(catch_key);
 
-
+    $('#login_submit').click(function(){
+      sign_in(function(){
+        $(document).unbind(event, catch_key);
+      })
+    });
   });
 
-  });
+  function sign_in(cb){
+    var username = $('#login_username').val();
+    var password = $('#login_password').val();
+    $.post(url + "test.php", {
+      email: username,
+      password: password
+    }, function(data) {
+      if(data == "Login failed"){
+        //failed
+        $("#login_feedback").html("Incorrect username or password");
+      }else{
+        //succeeded
+        $("#login_feedback").html("");
+        $('#header_username').html(username);
+        $('#main_container').show();
+        $('.login-container').fadeOut(300, function(){
+          $(this).hide();
+        });
+        if(typeof cb === "function") cb();
+      }
+      console.log(data);
+    });
 
 
+  }
 
 
 
