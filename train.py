a0ca14e53f1da31ee82b347a023156019bc3094d
GeorgeZJChen <35523752+GeorgeZJChen@users.noreply.github.com>
2019-01-15 10:55:55 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -4,37 +4,45 @@ import numpy as np
 import time
 import logging
 import pickle
+import random
 
 from _functions import *
 from model import *
 from test import *
+from data import *
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='0'
 
 tf.reset_default_graph()
 print("Initiating Tensors")
-graph = tf.Graph()
-with graph.as_default():
-  input = tf.placeholder( tf.float32, shape=[None, 384, 512, 3])
-  target15 = tf.placeholder( tf.float32 , shape=(None, 1, 1, 1))
-  target14 = tf.placeholder( tf.float32 , shape=(None, 3, 4, 1))
-  target13 = tf.placeholder( tf.float32 , shape=(None, 6, 8, 1))
-  target12 = tf.placeholder( tf.float32 , shape=(None, 12, 16, 1))
-  target11 = tf.placeholder( tf.float32 , shape=(None, 24, 32, 1))
-  target10 = tf.placeholder( tf.float32 , shape=(None, 48, 64, 1))
-  training = tf.placeholder( tf.bool )
-  alpha = tf.placeholder_with_default(tf.constant(1e-5, tf.float64), shape=[])
-  train_Gen, train_Dis, G_loss, D_loss, loss, Decoded, monitor = model(input, [target15, target14, target13, target12, target11, target10], training, alpha)
+# with tf.device('/device:GPU:2'):
+if True:
+  graph = tf.Graph()
+  with graph.as_default():
+    input = tf.placeholder( tf.float32, shape=[None, 384, 512, 3])
+    target15 = tf.placeholder( tf.float32 , shape=(None, 1, 1, 1))
+    target14 = tf.placeholder( tf.float32 , shape=(None, 3, 4, 1))
+    target13 = tf.placeholder( tf.float32 , shape=(None, 6, 8, 1))
+    target12 = tf.placeholder( tf.float32 , shape=(None, 12, 16, 1))
+    target11 = tf.placeholder( tf.float32 , shape=(None, 24, 32, 1))
+    target10 = tf.placeholder( tf.float32 , shape=(None, 48, 64, 1))
+    training = tf.placeholder( tf.bool )
+    dropout = tf.placeholder_with_default(tf.constant(0.3, tf.float32), shape=[])
+    alpha = tf.placeholder_with_default(tf.constant(1e-5, tf.float64), shape=[])
+    train, loss, Decoded, monitor = model(input, [target15, target14, target13, target12, target11, target10]
+                                                                       , training, alpha, dropout=dropout)
+    saver = tf.train.Saver(max_to_keep=10)
+    best_saver = tf.train.Saver(max_to_keep=4)
+    print('total number of parameters:', total_parameters())
 
-  saver = tf.train.Saver(max_to_keep=4)
-  print('total number of parameters:', total_parameters())
+new_model = False
+batch_size = 4
+part = 'A'
+best_result = 200
 
-new_model = True
-batch_size = 16
 logging.basicConfig(filename='./output/train.log',level=logging.INFO)
-with open('train_names.pkl', 'rb') as f:
-    train_names = pickle.load(f)
-with open('test_names.pkl', 'rb') as f:
-    test_names = pickle.load(f)
-test_names = np.array(test_names)
+train_names, test_names = get_train_data_names(part=part)
 
 print("Training begins")
 with tf.Session(graph=graph) as sess:
@@ -44,46 +52,31 @@ with tf.Session(graph=graph) as sess:
     global_step = 0
     EMA = 0
     train_MAEs = None
+    test_MAEs = None
   else:
-    saver.restore(sess, tf.train.latest_checkpoint('./model/'))
-#     saver.restore(sess, './model-ccmv2sha-12312249')
-    global_step = 7418
+    last_cp = tf.train.latest_checkpoint('./model')
+    saver.restore(sess, last_cp)
+#     saver.restore(sess, './model-ccmv2sha-01042236')
+    global_step = int(last_cp[last_cp.rfind('-')+1:])
     EMA = 0
     train_MAEs = None
+    test_MAEs = None
 
-  for step in range(100000):
+  try:
+    for step in range(200000):
+      if step < 50000:
+        lr = 1e-4
+      elif step < 100000:
+        lr = 1e-5
+      elif step < 150000:
+        lr = 1e-6
+      elif step < 200000:
+        lr = 1e-7
 
-    train_inputs, train_targets = next_batch(batch_size, train_names)
-    train_t15, train_t14, train_t13, train_t12, train_t11, train_t10 = train_targets
-    _ , train_loss_G, train_loss = sess.run([train_Gen, G_loss, loss], feed_dict={
-      input: train_inputs,
-      target15: train_t15,
-      target14: train_t14,
-      target13: train_t13,
-      target12: train_t12,
-      target11: train_t11,
-      target10: train_t10,
-      training: True,
-      alpha: 1e-5,
-    })
-    _ , train_loss_D,  = sess.run([train_Dis, D_loss], feed_dict={
-      input: train_inputs,
-      target15: train_t15,
-      target14: train_t14,
-      target13: train_t13,
-      target12: train_t12,
-      target11: train_t11,
-      target10: train_t10,
-      training: True,
-      alpha: 1e-5,
-    })
-    if EMA == 0:
-        EMA = train_loss_G
-    else:
-        EMA = moving_average(train_loss_G, EMA)
-    if step%100==0:
-
-      train_D = sess.run(Decoded, feed_dict={
+      train_inputs, train_targets = next_batch(batch_size, train_names)
+      train_t15, train_t14, train_t13, train_t12, train_t11, train_t10 = train_targets
+      random_dropout = random.random()*0.3
+      _ , train_loss = sess.run([train, loss], feed_dict={
           input: train_inputs,
           target15: train_t15,
           target14: train_t14,
@@ -92,36 +85,94 @@ with tf.Session(graph=graph) as sess:
           target11: train_t11,
           target10: train_t10,
           training: True,
+          alpha: lr,
+          dropout: random_dropout,
       })
-      train_out15, train_out14, train_out13, train_out12, train_out11, train_out10 = train_D
-
-      if train_MAEs is None:
-        train_MAEs = [ MAE(train_out15, train_t15), MAE(train_out14, train_t14), MAE(train_out13, train_t13), MAE(train_out12, train_t12), MAE(train_out11, train_t11), MAE(train_out10, train_t10)]
+      if EMA == 0:
+        EMA = train_loss
       else:
-        train_MAEs = moving_average_array([ MAE(train_out15, train_t15), MAE(train_out14, train_t14), MAE(train_out13, train_t13),
-                MAE(train_out12, train_t12), MAE(train_out11, train_t11), MAE(train_out10, train_t10)], train_MAEs)
+        EMA = moving_average(train_loss, EMA)
+      if step%10==0:
 
-      log_str = ['>>> TRAIN', time.asctime()[10:20]+': i [', str(global_step), '] || [loss, EMA]: [',
-                   str((round(train_loss, 2), round(train_loss_G-train_loss, 2), round(train_loss_D, 2)))+',', str(round(EMA,2)), '] || [EMAoMAE]:', str(train_MAEs)]
-      print(*log_str)
-      logging.info(' '.join(log_str))
+        train_D = sess.run(Decoded, feed_dict={
+            input: train_inputs,
+            target15: train_t15,
+            target14: train_t14,
+            target13: train_t13,
+            target12: train_t12,
+            target11: train_t11,
+            target10: train_t10,
+            training: True,
+            dropout: random_dropout,
+        })
+        train_out15, train_out14, train_out13, train_out12, train_out11, train_out10 = train_D
 
-      if step % 1000==0:
-          test_results = full_test(sess, Decoded,
-              input, target15, target14, target13, target12, target11, target10, training)
-          log_str = ['>>> TEST ', time.asctime()+': i [', str(global_step), '] || [Result]:', str(test_results)]
-          print(*log_str)
-          logging.info(' '.join(log_str))
+        test_inputs, test_targets = next_batch_test(batch_size, test_names)
+        test_t15, test_t14, test_t13, test_t12, test_t11, test_t10 = test_targets
+        test_D = sess.run(Decoded, feed_dict={
+            input: test_inputs,
+            target15: test_t15,
+            target14: test_t14,
+            target13: test_t13,
+            target12: test_t12,
+            target11: test_t11,
+            target10: test_t10,
+            training: False,
+        })
+        test_out15, test_out14, test_out13, test_out12, test_out11, test_out10 = test_D
 
-      if step%1000==0 and False:
+        if train_MAEs is None:
+          train_MAEs = [ MAE(train_out15, train_t15), MAE(train_out14, train_t14), MAE(train_out13, train_t13)
+                        , MAE(train_out12, train_t12), MAE(train_out11, train_t11), MAE(train_out10, train_t10)]
+        else:
+          train_MAEs = moving_average_array([ MAE(train_out15, train_t15), MAE(train_out14, train_t14), MAE(train_out13, train_t13),
+                  MAE(train_out12, train_t12), MAE(train_out11, train_t11), MAE(train_out10, train_t10)], train_MAEs)
+        if test_MAEs is None:
+          test_MAEs = [ MAE(test_out15, test_t15), MAE(test_out14, test_t14), MAE(test_out13, test_t13)
+                       , MAE(test_out12, test_t12), MAE(test_out11, test_t11), MAE(test_out10, test_t10)]
+        else:
+          test_MAEs = moving_average_array([MAE(test_out15,test_t15), MAE(test_out14,test_t14),
+                                            MAE(test_out13,test_t13), MAE(test_out12,test_t12),
+                  MAE(test_out11,test_t11), MAE(test_out10,test_t10)], test_MAEs)
 
-        display_set_of_imgs([train_out14[0], train_t14[0], train_out13[0], train_t13[0], train_out12[0]
-                             , train_t12[0], train_out11[0], train_t11[0], train_out10[0], train_t10[0], denormalize(train_inputs[0])]
-                             , rows=3, size=2, name='train-'+str(global_step))
+        log_str = ['>>> TRAIN', time.asctime()[10:20]+': i [', str(global_step), '] || [loss, EMA]: [',
+                   str(round(train_loss, 2))+', ', str(round(EMA,2)), '] || [EMAoMAE]:', str(train_MAEs)]
+        print(*log_str)
+        logging.info(' '.join(log_str))
 
-      if step%1000==0:
+        log_str = ['>>> TEST ', time.asctime()[10:20]+': i [', str(global_step), '] || [EMAoMAE]:', str(test_MAEs)]
+        print(*log_str)
+        logging.info(' '.join(log_str))
 
-        saver.save(sess, "./model/model", global_step=global_step)
-        print(">>> Model saved")
-        logging.info(">>> Model saved")
-    global_step = global_step + 1
+        if step%400==0 and False:
+
+          display_set_of_imgs([train_out14[0], train_t14[0], train_out13[0], train_t13[0], train_out12[0]
+                               , train_t12[0], train_out11[0], train_t11[0], train_out10[0], train_t10[0]
+                               , denormalize(train_inputs[0])], rows=3, size=2)
+          display_set_of_imgs([test_out14[0], test_t14[0], test_out13[0], test_t13[0], test_out12[0]
+                               , test_t12[0], test_out11[0], test_t11[0], test_out10[0], test_t10[0]
+                               , denormalize(test_inputs[0])], rows=3, size=2)
+
+        if step%200==0:
+
+          saver.save(sess, "./model/model", global_step=global_step)
+          print(">>> Model saved:", global_step)
+          logging.info(">>> Model saved: "+str(global_step))
+
+          if global_step>=2000 or step==0:
+            test_results = full_test(sess, Decoded,
+                input, target15, target14, target13, target12, target11, target10, training, part=part)
+            log_str = ['>>> TEST ', time.asctime()+': i [', str(global_step),
+                       '] || [Result]:', str([round(result, 2) for result in test_results])]
+            if test_results[0] > best_result:
+              best_result = test_results[0]
+              best_saver.save(sess, "./best_model/model-"+str(round(best_result,2)), global_step=global_step)
+            log_str.append(' * BEST *')
+            print(*log_str)
+            logging.info(' '.join(log_str))
+
+      global_step = global_step + 1
+  except KeyboardInterrupt:
+    print('>>> KeyboardInterrupt. Saving model...')
+    saver.save(sess, "./model/model", global_step=global_step)
+    print(">>> Model saved:", str(global_step))
